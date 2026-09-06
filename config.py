086fdc6de42157dca7026ca9cae61360784e0409
Dinesh Singh Dhami <dineshdhamidn@gmail.com>
2026-09-06 10:27:59 +0545

    Prepare application for Render deployment

--- config.py
+++ config.py
@@ -5,8 +5,15 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-later")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "dev-secret-key-change-later"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024
